493af3da2b0607f8928843aedbd3466fd5b39991
Philline <fidik@live.nl>
2018-11-07 13:20:28 +0100

    Initial project

--- app/src/main/java/com/example/phill/myapplication/Game.java
+++ app/src/main/java/com/example/phill/myapplication/Game.java
@@ -14,7 +14,6 @@ public class Game {
     public Game() {
         board = new TileState[BOARD_SIZE][BOARD_SIZE];
         for(int i=0; i<BOARD_SIZE; i++)
-
             for (int j = 0; j < BOARD_SIZE; j++)
                 board[i][j] = TileState.BLANK;
 
@@ -23,41 +22,41 @@ public class Game {
     }
 
 //      have a method that allows a user to “play
-        public TileState choose(int row, int column) {
+    public TileState choose(int row, int column) {
 
 //            If that place is still blank, we can go ahead and fill it.
-            if (board[row][column]== TileState.BLANK ){
+        if (board[row][column] == TileState.BLANK){
 
 //                if the current player is player one, fill it with a cross
-                if (playerOneTurn){
-                    board[row][column]= TileState.CROSS;
+            if (playerOneTurn){
+                board[row][column]= TileState.CROSS;
 //                    playerOneTurn = GameState.PLAYER_ONE;
-                    playerOneTurn = false;
+                playerOneTurn = false;
 
 //                    Check if player has won the game
-                    return  TileState.CROSS;
+                return  TileState.CROSS;
 
-                }
+            }
 
 //                if the current player is player two, fill it with a circle
-                else{
-                    board[row][column]= TileState.CIRCLE;
-                    playerOneTurn = true;
+            else{
+                board[row][column]= TileState.CIRCLE;
+                playerOneTurn = true;
 
 
 //                    Check if player has won the game
 
-                    return  TileState.CIRCLE;
-                }
+                return  TileState.CIRCLE;
             }
+        }
 
 //            it’s an invalid move! Just return TileState.INVALID.
-            else {
-                return TileState.INVALID;
-            }
-
-
+        else {
+            return TileState.INVALID;
         }
+
+
+    }
 
 //    This method should verify the state of the board
 //    return an appropriate value of the GameState enum
@@ -66,25 +65,47 @@ public class Game {
         for(int i=0; i<BOARD_SIZE; i++)
 
 //            check if horizontal win
-            if ( board[i][0] == board[i][1]  &&   board[i][0] == board[i][3]) {
-                return GameState.WIN;
+            if ( board[i][0] == board[i][1]  &&   board[i][0] == board[i][2] && board[i][0] != TileState.BLANK) {
+                if (board[i][0] == TileState.CROSS){
+                    return GameState.PLAYER_ONE;
+                }
+                else{
+                    return GameState.PLAYER_TWO;
+                }
             }
 
 //            check if vertical win
-            else if  ( board[0][i] == board[1][i] &&  board[0][i] == board[3][i]) {
-                return GameState.WIN;
+            else if  ( board[0][i] == board[1][i] &&  board[1][i] == board[2][i] && board[0][i] != TileState.BLANK) {
+                if (board[0][i] == TileState.CROSS){
+                    return GameState.PLAYER_ONE;
+                }
+                else{
+                    return GameState.PLAYER_TWO;
+                }
+
             }
 
 //            check if schuin vanuit links win
-            else if  ( board[0][0] == board[1][1]  && board[0][0] == board[3][3]) {
-                return GameState.WIN;
+            else if  ( board[0][0] == board[1][1]  && board[1][1] == board[2][2] && board[0][0] != TileState.BLANK) {
+                if (board[0][0] == TileState.CROSS){
+                    return GameState.PLAYER_ONE;
+                }
+                else{
+                    return GameState.PLAYER_TWO;
+                }
+
             }
 
 //            check if schuin vanuit rechts win
-            else if  ( board[0][3] == board[1][1]  && board[0][3] == board[3][1] {
-                return GameState.WIN;
-            }
+            else if  ( board[0][2] == board[1][1]  && board[1][1] == board[2][0]&& board[0][2] != TileState.BLANK) {
+                if (board[0][2] == TileState.CROSS){
+                    return GameState.PLAYER_ONE;
+                }
+                else{
+                    return GameState.PLAYER_TWO;
+                }
 
+            }
 
 //            check if draw
         for(int i=0; i<BOARD_SIZE; i++){
@@ -96,6 +117,7 @@ public class Game {
         }
         //         because neither player has won but all tiles have been claimed
         if (count == (BOARD_SIZE*BOARD_SIZE)){
+
                 return GameState.DRAW;
         }
         else{
@@ -103,8 +125,6 @@ public class Game {
         }
 
     }
-
-
 
 }
 
--- app/src/main/java/com/example/phill/myapplication/MainActivity.java
+++ app/src/main/java/com/example/phill/myapplication/MainActivity.java
@@ -4,19 +4,37 @@ package com.example.phill.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
 
 
 public class MainActivity extends AppCompatActivity {
     Game game;
     int row;
     int column;
+    final private int BOARD_SIZE = 3;
+    private TileState[][] board;
+
+    Button a1, a2, a3 , b1, b2, b3, c1, c2, c3;
+    TextView Win_Text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         game = new Game();
+        a1 = findViewById(R.id.Button1);
+        a2 = findViewById(R.id.Button2);
+        a3 = findViewById(R.id.Button3);
+        b1 = findViewById(R.id.Button4);
+        b2 = findViewById(R.id.Button5);
+        b3 = findViewById(R.id.Button6);
+        c1 = findViewById(R.id.Button7);
+        c2 = findViewById(R.id.Button8);
+        c3 = findViewById(R.id.Button9);
+
+
     }
 
 //    public void buttonClicked(View view) {
@@ -27,91 +45,109 @@ public class MainActivity extends AppCompatActivity {
 
         // First step is to translate the button ID into coordinates
         // to find out which button it is
-        int id = view.getId();
-        if (id == 1){
-            row = 1;
-            column = 1;
-        }
-        else if (id == 2){
-            row =1;
-            column = 2;
-        }
-        else if (id == 3){
-            row =1;
-            column = 3;
-        }
-        else if (id == 4){
-            row =2;
-            column = 1;
-        }
-        else if (id == 5){
-            row =2;
-            column = 2;
-        }
-        else if (id == 6){
-            row =2;
-            column = 3;
-        }
-        else if (id == 7){
-            row =3;
-            column = 1;
-        }
-        else if (id == 8){
-            row =3;
-            column = 2;
-        }
-        else if (id == 9){
-            row =3;
-            column = 3;
-        }
-        // store the corresponding row and column
-
-        // moet deze coördinaten aan de spelkeuzemethode toevoegen
-        TileState state = game.choose(row, column);
-
-        // String to fill in the button
-        String button = "";
-
-        // Depending on the outcome of the choose method, it has to update the selected button. Here’s a starter:
-        switch(state) {
-            case CROSS:
-                // Make the button a cross
-                button = "X";
-                break;
-            case CIRCLE:
-
-                // Make the button a circle
-                button = "O";
-                break;
-            case INVALID:
-
-                // There is already something in the checkbox
-                button = "";
-                break;
-        }
-
         GameState finish = game.won();
 
         switch (finish){
             case IN_PROGRESS:
-                break;
-            case PLAYER_ONE:
-                break;
-            case PLAYER_TWO:
-                break;
+                int id = view.getId();
+                Button button = (Button) view;
+
+                if (id == a1.getId()){
+                    row = 0;
+                    column = 0;
+                }
+                else if (id == a2.getId()){
+                    row =0;
+                    column = 1;
+                }
+                else if (id == a3.getId()){
+                    row =0;
+                    column = 2;
+                }
+                else if (id == b1.getId()){
+                    row =1;
+                    column = 0;
+                }
+                else if (id == b2.getId()){
+                    row =1;
+                    column = 1;
+                }
+                else if (id == b3.getId()){
+                    row =1;
+                    column = 2;
+                }
+                else if (id == c1.getId()){
+                    row =2;
+                    column = 0;
+                }
+                else if (id == c2.getId()){
+                    row =2;
+                    column = 1;
+                }
+                else if (id == c3.getId()){
+                    row =2;
+                    column = 2;
+                }
+                // store the corresponding row and column
+                TileState state = game.choose(row, column);
+
+                // String to fill in the button
+                String but = "";
+
+                // Depending on the outcome of the choose method, it has to update the selected button. Here’s a starter:
+                switch(state) {
+                    case CROSS:
+
+                        // Make the button a cross
+                        but = "X";
+                        break;
+                    case CIRCLE:
+
+                        // Make the button a circle
+                        but= "0";
+                        break;
+                    case INVALID:
+
+                        // There is already something in the checkbox
+
+                        break;
+                }
+                TextView b = (TextView) view;
+                b.setText(but);
+
             case DRAW:
+
+//                Win_Text = findViewById(R.id.textView);
+//                Win_Text.setText("Nobody won the game, click on RESET to start a new game");
                 break;
-            case WIN:
+
+//             When player one has won
+            case PLAYER_ONE:
+                Win_Text = findViewById(R.id.textView);
+                Win_Text.setText("Player one has won");
                 break;
+//            When player two has won
+            case PLAYER_TWO:
+                Win_Text = findViewById(R.id.textView);
+                Win_Text.setText("Player two has won");
+                break;
+
         }
 
 
-
-        TextView b = (TextView) view;
-        b.setText(button);
-
     }
     public void resetClicked(View view) {
+        Win_Text = findViewById(R.id.textView);
+        Win_Text.setText("");
+        a1.setText("");
+        a2.setText("");
+        a3.setText("");
+        b1.setText("");
+        b2.setText("");
+        b3.setText("");
+        c1.setText("");
+        c2.setText("");
+        c3.setText("");
         game = new Game();
 //        we can simply throw away the old game and create a new one
 //         update user interface
